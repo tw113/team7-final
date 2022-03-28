8d7f73bbaf19f30acd68b25cb216580545828ba0
Anna Astle <ast19004@byui.edu>
2022-03-28 11:55:55 -0700

    jwt token added at login

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,15 @@
 // Controller for Users
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); //MODIFIED / ADDED -> make sure to npm install --save bcrypt
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-// const validationResult = require("express-validator"); // MODIFIED - commented out -> not using yet
+// const validationResult = require("express-validator"); //
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.postSignup = (req, res, next) => {
-  const firstName = req.body.firstName; // MODIFIED / ADDED
-  const lastName = req.body.lastName; // MODIFIED / ADDED
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
   const email = req.body.email;
   const password = req.body.password;
 
@@ -22,16 +23,16 @@ exports.postSignup = (req, res, next) => {
     .hash(password, 12)
     .then((hashedPassword) => {
       const user = new User({
-        firstName: firstName, // MODIFIED / ADDED
-        lastName: lastName, // MODIFIED / ADDED
+        firstName: firstName,
+        lastName: lastName,
         email: email,
         password: hashedPassword,
-        recipes: [], // MODIFIED cart: { items: [] }, to recipes: []
+        recipes: [],
       });
       return user.save();
     })
     .then((result) => {
-      res.status(201).json({ message: 'User Added Successfully.' }); // MODIFIED / ADDED
+      res.status(201).json({ message: "User Added Successfully." }); // MODIFIED / ADDED
     })
     .catch((err) => {
       const error = new Error(err);
@@ -44,24 +45,34 @@ exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  User
-    .findOne({ email: email })
+  User.findOne({ email: email })
     .then((user) => {
       if (user) {
-        bcrypt
-          .compare(password, user.password)
-          .then((result) => {
-            if (result) {
-              res.status(200).json({ message: 'Successfully Logged In', user: user });
-            } else {
-              res.status(400).json({ message: 'Invalid user information' });
-            }
+        bcrypt.compare(password, user.password).then((result) => {
+          if (result) {
+            //Create new signature packed into jsonwebtoken
+            const token = jwt.sign(
+              {
+                userId: user._id.toString(),
+                email: user.email,
+              },
+              "somesupersecretsecret",
+              { expiresIn: "1h" }
+            );
+            res.status(200).json({
+              message: "Successfully Logged In",
+              userId: user._id.toString(),
+              token: token,
+            });
+          } else {
+            res.status(400).json({ message: "Invalid user information" });
+          }
         });
       } else {
-        res.status(401).json({ message: 'User not found' });
+        res.status(401).json({ message: "User not found" });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
