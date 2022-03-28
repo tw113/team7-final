0f703b8bed6d315ae827f1a452d978e6de5a26b9
Anna Astle <ast19004@byui.edu>
2022-03-28 10:16:50 -0700

    Recipe Router: Added delete route

--- routes/recipe.js
+++ routes/recipe.js
@@ -9,8 +9,10 @@ router.get("/recipes", recipeController.getRecipes);
 
 router.get("/recipe/:recipeId", recipeController.getRecipe);
 
-router.post("/recipe", /*isAuth,*/ recipeController.postRecipe); // MODIFIED router.post("/recipes", recipeController.postRecipe);
+router.post("/recipe", /*isAuth,*/ recipeController.postRecipe);
 
-router.put("/recipe/:recipeId", recipeController.putEditRecipe);
+router.put("/recipe/:recipeId", /* isAuth,*/ recipeController.putEditRecipe);
+
+router.delete("/recipe/:recipeId", /* isAuth,*/ recipeController.deleteRecipe);
 
 module.exports = router;
