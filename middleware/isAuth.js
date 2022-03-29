0281c45bc730c8b945745c566582706719b93a64
Anna Astle <ast19004@byui.edu>
2022-03-29 11:51:32 -0700

    jwt swagger authorization working!

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
     error.statusCode = 401;
     throw error;
   }
-  const token = authHeader.split("")[1];
+  const token = authHeader.split(" ")[1];
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, secret);
--- routes/recipe.js
+++ routes/recipe.js
@@ -9,10 +9,10 @@ router.get("/recipes", recipeController.getRecipes);
 
 router.get("/recipe/:recipeId", recipeController.getRecipe);
 
-router.post("/recipe", /*isAuth,*/ recipeController.postRecipe);
+router.post("/recipe", isAuth, recipeController.postRecipe);
 
-router.put("/recipe/:recipeId", /* isAuth,*/ recipeController.putEditRecipe);
+router.put("/recipe/:recipeId", isAuth, recipeController.putEditRecipe);
 
-router.delete("/recipe/:recipeId", /* isAuth,*/ recipeController.deleteRecipe);
+router.delete("/recipe/:recipeId", isAuth, recipeController.deleteRecipe);
 
 module.exports = router;
