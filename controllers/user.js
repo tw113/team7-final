3085e29c8d7601f809f6f63aa29da226240c9989
Anna Astle <ast19004@byui.edu>
2022-03-29 21:20:13 -0700

    Added validation for postLogin

--- controllers/user.js
+++ controllers/user.js
@@ -47,6 +47,15 @@ exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).json({
+      errorMessage: errors.array()[0].errorMessage,
+      validationErrors: errors.array(),
+    });
+  }
+
   User.findOne({ email: email })
     .then((user) => {
       if (user) {
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 // user routes
+const User = require("../models/user");
 
 const express = require("express");
-const { check, body } = require('express-validator');
+const { check, body } = require("express-validator");
 
 const userController = require("../controllers/user");
 
@@ -10,38 +11,53 @@ const router = express.Router();
 router.post(
   "/user/register",
   [
-    check('email')
+    check("firstName").isLength({ min: 1 }).trim(),
+    check("lastName").isLength({ min: 1 }).trim(),
+    check("email")
       .isEmail()
-      .withMessage('Please enter a valid email.')
+      .withMessage("Please enter a valid email.")
       .custom((value, { req }) => {
-        return User.findOne({ email: value }).then(userDoc => {
+        return User.findOne({ email: value }).then((userDoc) => {
           if (userDoc) {
             return Promise.reject(
-              'E-Mail exists already, please pick a different one.'
+              "E-Mail exists already, please pick a different one."
             );
           }
         });
       })
       .normalizeEmail(),
-      body(
-        'password',
-        'Please enter a password with only numbers and text and at least 5 characters.'
-      )
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    )
       .isLength({ min: 5 })
       .isAlphanumeric()
       .trim(),
-      body('confirmPassword')
+    body("confirmPassword")
       .trim()
       .custom((value, { req }) => {
         if (value !== req.body.password) {
-          throw new Error('Passwords have to match!');
+          throw new Error("Passwords have to match!");
         }
         return true;
-      })
+      }),
   ],
   userController.postSignup
 );
 
-router.post("/user/login", userController.postLogin);
+router.post(
+  "/user/login",
+  [
+    check("email").isEmail().withMessage("Please enter a valid email."),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+  ],
+  userController.postLogin
+);
 
 module.exports = router;
